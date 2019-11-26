76e48ab0095dda3139bb801c2a98359dc9190ba7
phungvankhanh <vankhanhbk14@gmail.com>
2019-11-26 11:27:29 +0900

    draw basic layout with 2 tabs

---
+++ _notes.md
@@ -0,0 +1,5 @@
+# Color Theme
+
+Primary: purple[700]
+Accent: purpleAccent[100]
+Secondary: blueGray[700]
--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sparkline/flutter_sparkline.dart';
 
-void main() => runApp(MyApp());
+import 'portfolio.dart';
+import 'market.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(new MaterialApp(
+    color: Colors.orange,
+    title: "CryptoTracking",
+    home: new Tabs(),
+    routes: <String, WidgetBuilder> {},
+    theme: new ThemeData(
+      primaryColor: Colors.purple[700],
+      accentColor: Colors.purpleAccent[100],
+      textSelectionColor: Colors.grey[700],
+      brightness: darkEnabled ? Brightness.dark : Brightness.light,
+    ),
+  ));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+bool darkEnabled = true;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+const double appBarHeight= 50.0;
+final double appBarElevation = 0.5;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Tabs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return new DefaultTabController(
+      length: 2,
+      child: new Scaffold(
+        bottomNavigationBar: new BottomAppBar(
+          elevation: 4.0,
+          child: new Container(
+            height: 34.0,
+            child: new TabBar(
+              indicatorColor: Theme.of(context).primaryColor,
+              indicatorPadding: const EdgeInsets.only(left: 60.0, bottom: 2.0, right: 60.0),
+              tabs: <Tab>[
+                new Tab(icon: new Icon(Icons.person_outline, color: Theme.of(context).primaryColor)),
+                new Tab(icon: new Icon(Icons.trending_up, color: Theme.of(context).primaryColor,),),
+              ],
+            ),
+          ),
+        ),
+        body: new TabBarView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            new PortfolioPage(),
+            new MarketPage(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
